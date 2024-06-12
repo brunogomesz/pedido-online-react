5fad026f17ef7259af299438c4438e6d49f2d5b9
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-12 16:09:22 -0300

    Acrescentando os demais icones

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Container } from './styles'
 
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 import menuImg from '../../assets/menu.svg'
 
@@ -17,6 +20,24 @@ export function Sidebar() {
             <a href=" " className="active">
               <BurgerIcon />
               <span>Hambúrgueres</span>
+            </a>
+          </li>
+          <li>
+            <a href=" ">
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+          <li>
+            <a href=" ">
+              <SodaPopIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+          <li>
+            <a href=" ">
+              <IceCreamIcon />
+              <span>Sorvetes</span>
             </a>
           </li>
         </ul>
