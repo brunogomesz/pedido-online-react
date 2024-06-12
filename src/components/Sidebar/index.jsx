e3bb6563e56309f68a55092a5315be49a1711628
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-12 15:07:38 -0300

    Criando o elemento de navegação

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,26 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container } from './styles'
 
-export function Sidebar(){
-  return <Container>Sidebar</Container>
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+
+export function Sidebar() {
+  return (
+    <Container>
+      <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar o menu' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href=" ">
+              <BurgerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
 }
