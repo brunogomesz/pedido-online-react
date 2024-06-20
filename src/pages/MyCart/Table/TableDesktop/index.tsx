6d174e282efe22627509f3afaf5f95637d10f3a6
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-20 17:47:08 -0300

    Implementando o incremento e decremento de Snack

--- src/pages/MyCart/Table/TableDesktop/index.tsx
+++ src/pages/MyCart/Table/TableDesktop/index.tsx
@@ -9,7 +9,7 @@ import plusImg from '../../../../assets/circle-plus.svg'
 import { Container } from './styles'
 
 export function TableDesktop() {
-  const { cart, removeSnackFromCart } = useCart()
+  const { cart, removeSnackFromCart, snackCartIncrement, snackCartDecrement } = useCart()
 
   return (
     <Container>
@@ -35,11 +35,11 @@ export function TableDesktop() {
               </td>
               <td>
                 <div>
-                  <button type='button' onClick={() => console.log(`decrementar snack`, item)}>
+                  <button type='button' onClick={() => snackCartDecrement(item)}>
                     <img src={minusImg} alt='Remover quantidade' />
                   </button>
                   <span>{`${item.quantity}`.padStart(2, '0')}</span>
-                  <button type='button' onClick={() => console.log(`incrementar snack`, item)}>
+                  <button type='button' onClick={() => snackCartIncrement(item)}>
                     <img src={plusImg} alt='Adicionar quantidade' />
                   </button>
                 </div>
