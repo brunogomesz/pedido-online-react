945bf5faa1da714f96bcf2f405b7d8cee3fb1fa9
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-20 13:59:40 -0300

    Implementando o badge de quantidade de itens

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -11,29 +11,35 @@ interface SnacksProps {
 }
 
 export function Snacks({ snacks }: SnacksProps){
-  const { addSnackIntoCart } = useCart()
+  const { cart, addSnackIntoCart } = useCart()
 
 
   return (
     <Container>
       {/* Em um ambiente normal de JavaScript colocariamos a {} depois do => porque vai vim uma função em seguida, mas em jsx colocamos () */}
-      {!snacks.length ? ( // carrega o Skeleton de loading
-        [1,2,3,4].map((n) => <SkeletonSnack key={n} />)
-      ) : (
-        snacks.map((snack) => (
-          <div key={snack.id} className='snack'>
-            <h2>{snack.name}</h2>
-            <img src={snack.image} alt={snack.name} />
-            <p>{snack.description}</p>
-            <div>
-              <strong>{currencyFormat(snack.price)}</strong>
-              <button type='button' onClick={() => addSnackIntoCart(snack)}>
-                <FiPlus />
-              </button>
-            </div>
-          </div>
-        ))
-      )}
+      {!snacks.length
+        ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+        : snacks.map((snack) => { // para executar qualquer código JavaScript dentro da função, precisa colocar o return, mas só executa o código se for colocado a chave "{}"
+            const snackExistent = cart.find(
+              (item) => item.snack === snack.snack && item.id === snack.id,
+            )
+
+            return ( // refere-se a um retorno do jsx
+              <div key={snack.id} className='snack'>
+                {/* bolinha que mostra a quantidade de snack selecionada */}
+                {snackExistent && <span>{snackExistent.quantity}</span>}
+                <h2>{snack.name}</h2>
+                <img src={snack.image} alt={snack.name} />
+                <p>{snack.description}</p>
+                <div>
+                  <strong>{currencyFormat(snack.price)}</strong>
+                  <button type='button' onClick={() => addSnackIntoCart(snack)}>
+                    <FiPlus />
+                  </button>
+                </div>
+              </div>
+            )
+          })}
     </Container>
   )
 }
