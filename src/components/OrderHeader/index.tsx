f42ca9c53839edd59d17d3ee9a31805ce16b94ed
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-20 15:31:11 -0300

    Ajustando o Header do Cart - Itens

--- src/components/OrderHeader/index.tsx
+++ src/components/OrderHeader/index.tsx
@@ -1,14 +1,29 @@
 import { Link } from 'react-router-dom'
+
+import { useCart } from '../../hooks/useCart'
+
 import logoImg from '../../assets/logo.svg'
+import { ReactComponent as CartIcon } from '../../assets/shopping-cart.svg'
 
 import { Container } from './styles'
 
 export function OrderHeader() {
+  const { cart } = useCart()
+
   return (
     <Container>
       <Link to='/'> {/* vai para o endereço principal */}
         <img src={logoImg} alt='Food Commerce' />
       </Link>
+      <div>
+        <div>
+          <h3>Meus pedidos</h3>
+          <span>
+            <strong>{`${cart.length}`.padStart(2, '0')}</strong> lanche(s)
+          </span>
+        </div>
+        <CartIcon />
+      </div>
     </Container>
   )
 }
