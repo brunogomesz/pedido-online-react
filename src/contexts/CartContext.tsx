771b40268a4de4ef32f0816e629ba35b3d21b9df
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-20 09:43:23 -0300

    Adicionando as primeiras funções do carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -21,8 +21,8 @@ interface UpdateCartProps {
 interface CartContextProps {
   cart: Snack[]
   addSnackIntoCart: (snack: SnackData) => void // função de adicionar no carrinho - void retorna vazio, não retorna nada
-  removeSnackFromCart: ({id, snack}: RemoveSnackFromCart) => void
-  updateCart: ({id, snack, newQuantity}: UpdateCartProps) => void
+  // removeSnackFromCart: ({id, snack}: RemoveSnackFromCart) => void
+  // updateCart: ({id, snack, newQuantity}: UpdateCartProps) => void
 }
 
 interface CartProviderProps {
@@ -32,5 +32,21 @@ interface CartProviderProps {
 export const CartContext = createContext({} as CartContextProps)
 
 export function CartProvider({ children }: CartProviderProps){
-  //
+  const [cart, setCart] = useState<Snack[]>([])
+
+  //função de adicionar
+  function addSnackIntoCart (snack: SnackData): void {
+    // função que monta um novo snack baseado no snack que tem como parametro (SnackData)
+    const newSnack = {...snack, quantity: 1, subtotal:snack.price} // construir um novo array, incorporando os atributos do snack
+    //adiciona o snack no cart existente
+    const newCart = [...cart, newSnack] // push de um array, coloca o novo cart no final da fila
+
+    setCart(newCart)
+  }
+
+  return (
+    <CartContext.Provider value={{ cart, addSnackIntoCart}}>
+      {children}
+    </CartContext.Provider>
+  )
 }
