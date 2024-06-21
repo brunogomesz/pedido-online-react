efcde6d276435e9ccda9013ff4b27d6ce0676580
Bruno Gomes <brunogomesz@hotmail.com>
2024-06-21 11:08:52 -0300

    Criando as bases para o formulário

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,14 +1,40 @@
 import { Head } from '../../components/Head'
 import { OrderHeader } from '../../components/OrderHeader'
 
-import { Container, Inner } from './styles'
+import { Container, Form, Inner } from './styles'
 
 export default function Payment() {
   return (
     <Container>
-      <Head title='Pagamento' />
-      <OrderHeader />
-      <Inner>Formulário</Inner>
-    </Container>
+    <Head title='Pagamento' />
+    <OrderHeader />
+    <Inner>
+      <Form>
+        <h4>Informações pessoais</h4>
+
+        <div className='field'>
+          <label htmlFor='full-name'>Nome e sobrenome</label>
+          <input type='text' id='full-name' name='full-name' autoComplete='name' />
+        </div>
+
+        <div className='grouped'>
+          <div className='field'>
+            <label htmlFor='email'>E-mail</label>
+            <input type='email' name='email' id='email' autoComplete='email' />
+          </div>
+
+          <div className='field'>
+            <label htmlFor='mobile'>Celular</label>
+            <input type='tel' id='mobile' name='mobile' autoComplete='phone' />
+          </div>
+
+          <div className='field'>
+            <label htmlFor='document'>CPF / CNPJ</label>
+            <input type='text' id='document' name='document' />
+          </div>
+        </div>
+      </Form>
+    </Inner>
+  </Container>
   )
 }
